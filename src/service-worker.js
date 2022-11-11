e3b74db7405239a57da09feb7fcacc37e5d73546
orianne-grd <84494983+orianne-grd@users.noreply.github.com>
2022-11-12 00:32:45 +0100

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -50,9 +50,9 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png') || url.pathname.endsWith('.js'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
-    cacheName: 'images',
+    cacheName: 'data',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
@@ -61,15 +61,6 @@ registerRoute(
   })
 );
 
-registerRoute("data/*.js",
-  new StaleWhileRevalidate({
-    cacheName: 'datas',
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
-    ],
-  }));
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
